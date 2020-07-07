3d69a4e90556b1b4dfde79fed444ff8cc8942244
samsonl <lyndon.samson@gmail.com>
2020-07-07 17:30:33 +1000

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,5 +1,16 @@
+allow_scm_jenkinsfile=true
+
+application_environments {
+  global {
+     merge = true
+     A = "jteconfg A"
+  }
+}
 
 libraries {
+   merge = true 
+  override=true
+  
   libcommon
   libios
 }
